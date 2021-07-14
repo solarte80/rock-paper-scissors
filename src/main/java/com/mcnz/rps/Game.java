997abeae37e2c8c3c23537e5117c078adce15a31
solarte80 <39067235+solarte80@users.noreply.github.com>
2021-07-13 23:38:22 -0300

    Update Game.java

--- src/main/java/com/mcnz/rps/Game.java
+++ src/main/java/com/mcnz/rps/Game.java
@@ -1,3 +1,17 @@
+/**
+ * This module is about imp
+ act of the final keyword on performance
+ * <p>
+ * This module explores  if there are any performance benefits from
+ * using the final keyword in our code. This module examines the performance
+ * implications of using final on a variable, method, and class level.
+ * </p>
+ *
+ * @since 1.0
+ * @author baeldung
+ * @version 1.1
+ */
+
 package com.mcnz.rps;
 
 import javax.swing.JOptionPane;
